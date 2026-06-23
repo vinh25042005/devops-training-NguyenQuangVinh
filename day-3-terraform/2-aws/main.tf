803932a645cc0ace408fe56c0d3f42d6056f4b7c
Vinh <vinh2504123@gmail.com>
2026-06-23 07:29:25 +0000

    feat: thêm 2 subnets ở 2 AZ khác nhau

--- day-3-terraform/2-aws/main.tf
+++ day-3-terraform/2-aws/main.tf
@@ -19,3 +19,18 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
   tags = { Name = "devops-training-vpc" }
 }
+
+# Public subnets
+locals {
+  azs = ["ap-southeast-1a", "ap-southeast-1b"]
+}
+
+resource "aws_subnet" "public" {
+  count             = length(local.azs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 1)
+  availability_zone = local.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = { Name = "devops-training-public-${count.index + 1}" }
+}
