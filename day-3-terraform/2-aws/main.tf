f28e726d2d71a7c85a933b1ef21f99ee27151b3c
Vinh <vinh2504123@gmail.com>
2026-06-23 07:56:45 +0000

    feat: thêm security group

--- day-3-terraform/2-aws/main.tf
+++ day-3-terraform/2-aws/main.tf
@@ -20,7 +20,6 @@ resource "aws_vpc" "main" {
   tags = { Name = "devops-training-vpc" }
 }
 
-# Public subnets
 locals {
   azs = ["ap-southeast-1a", "ap-southeast-1b"]
 }
@@ -57,4 +56,36 @@ resource "aws_route_table_association" "public" {
   count          = length(local.azs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+# Security Group 
+resource "aws_security_group" "web" {
+  name        = "devops-training-web-sg"
+  description = "Allow SSH + HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH tu IP cua minh" 
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    description = "HTTP tu bat ky dau"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "devops-training-web-sg" }
 }
