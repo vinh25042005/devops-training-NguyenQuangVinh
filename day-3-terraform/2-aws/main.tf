e434bdac5e09b9b269d9ac949d38d6c73b078d4f
Vinh <vinh2504123@gmail.com>
2026-06-23 07:48:51 +0000

    feat: thêm IGW + route table

--- day-3-terraform/2-aws/main.tf
+++ day-3-terraform/2-aws/main.tf
@@ -34,3 +34,27 @@ resource "aws_subnet" "public" {
 
   tags = { Name = "devops-training-public-${count.index + 1}" }
 }
+
+# IGW
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "devops-training-igw" }
+}
+
+# Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = { Name = "devops-training-public-rt" }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(local.azs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
