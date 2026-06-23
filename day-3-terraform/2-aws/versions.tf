1659d292045ecf9d181a6c1333b38f6d7394012c
Vinh <vinh2504123@gmail.com>
2026-06-23 08:29:09 +0000

    feat: thêm s3 remote backend

--- day-3-terraform/2-aws/versions.tf
+++ day-3-terraform/2-aws/versions.tf
@@ -6,4 +6,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket         = "tfstate-vinh-1782202788"
+    key            = "phase1/week2/day3.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "tfstate-lock"
+    encrypt        = true
+  }
 }
